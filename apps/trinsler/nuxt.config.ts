1602fadd1cdf711925795caa4fb37ffe7767cffb
Nielshen <niels.hensler@gmail.com>
2025-09-15 22:50:51 +0200

    fix missing insights

--- apps/trinsler/nuxt.config.ts
+++ apps/trinsler/nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  nitro: {
+    prerender: {
+      failOnError: false,
+      crawlLinks: true
+    }
+  }
 })
