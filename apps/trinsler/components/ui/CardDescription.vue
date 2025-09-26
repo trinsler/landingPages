74f5663ec0ef5460b8fd909f5737b2bf8f61d432
Nielshen <niels.hensler@gmail.com>
2025-09-26 09:04:20 +0200

    trinsler migrated to shadcn

---
+++ apps/trinsler/components/ui/CardDescription.vue
@@ -0,0 +1,15 @@
+<template>
+  <p :class="cn('text-sm text-muted-foreground', props.class)" v-bind="$attrs">
+    <slot />
+  </p>
+</template>
+
+<script setup lang="ts">
+import { cn } from '~/lib/utils'
+
+interface CardDescriptionProps {
+  class?: string
+}
+
+const props = withDefaults(defineProps<CardDescriptionProps>(), {})
+</script>
